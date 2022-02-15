f7e8e8342e1fe61114e87c5841b621801607f492
lunata <nataly@krc.karelia.ru>
2022-02-15 13:38:17 +0300

    added Selsovets controller and Selsovet model

---
+++ app/Models/Dict/Selsovet1926.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Dict;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Selsovet1926 extends Model
+{
+    use HasFactory;
+}
