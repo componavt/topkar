9721732b41bfa2e45c673f3d281fde9f2db3a839
lunata <nataly@krc.karelia.ru>
2023-03-24 11:36:14 +0300

    lang migration

---
+++ app/Models/Models/Dict/Lang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Models\Dict;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lang extends Model
+{
+    use HasFactory;
+}
