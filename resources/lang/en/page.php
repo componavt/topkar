633a60a7ff089c56e020e4cf5e84787225f86972
lunata <nataly.krizhanovsky@gmail.com>
2023-06-17 09:51:32 +0300

    first page text

--- resources/lang/en/page.php
+++ resources/lang/en/page.php
@@ -14,6 +14,41 @@ return [
     */
 
     'site_title'  => 'TopKar - Toponymy of Karelia',
-    'welcome_text'  => 'Welcome to TopKar!',
     
+    'welcome_text_intro'  => 'Welcome to the geoinformation resource on the toponymy of Karelia (TopKar), '
+    . 'developed by a group of researchers from the Karelian Scientific Center of the Russian Academy of Sciences. '
+    . 'This work was supported by the Russian Science Foundation '
+    . '<a href="https://rscf.ru/project/22-28-00362/">grant no. 22-28-00362</a>.',
+    
+    'welcome_text_content'  => 'The TopKar resource (Toponymy of Karelia) allows you to save, systematize and explore '
+    . '<a href="http://topkar.krc.karelia.ru/en/dict/toponyms">the names of geographical objects</a> in Karelia and adjacent areas. '
+    . 'Toponyms are presented in Karelian, Veps and Russian (both are given if there are official and local variants of the name). '
+    . 'Each name is linked to the location of named natural objects — region, district and settlement. '
+    . 'Currently, a mechanism is being developed to display the presented toponyms on electronic maps. '
+    . 'It is possible to conduct an etymological and structural analysis of the names of natural objects.',
+        
+    'welcome_reference_tables'  => 'To narrow the search area, specify the data from the reference tables: '
+    . '<a href="http://topkar.krc.karelia.ru/en/dict/regions">regions</a>, '
+    . '<a href="http://topkar.krc.karelia.ru/en/dict/districts">districts</a>, '
+    . '<a href="http://topkar.krc.karelia.ru/en/dict/settlements">settlements</a>, '
+    . 'both modern and '
+    . '<a href="http://topkar.krc.karelia.ru/en/dict/settlements1926">according to the data for 1926</a>. '
+    . 'It is also possible to search for toponyms using '
+    . '<a href="http://topkar.krc.karelia.ru/en/misc/informants">the lists of informants</a> and '
+    . '<a href="http://topkar.krc.karelia.ru/en/misc/recorders">recorders</a>.',
+    
+    'welcome_text_sources'  => 'Researchers of the Karelian Scientific Center collect toponymic and folklore materials during expeditions, '
+    . 'decipher them and upload them to the TopKar database. '
+    . 'The Scientific Card Index of Toponyms of Karelia and Adjacent Regions '
+    . 'The Institute of Language, Literature and History of Karelian Research Center RAS (ILLH KarRC RAS), '
+    . 'the Scientific Archive of KarRC RAS and the Phonogram Archive of the ILLH KarRC RAS '
+    . 'are the sources for the database.',
+    
+    'welcome_text_software'  => 'Website and software TopKar is an open source project '
+    . '(see <a href="https://github.com/componavt/topkar">topkar</a> on GitHub) developed at KarRC RAS. '
+    . 'The TopKar system is designed for toponymists '
+    . 'who study the toponymy of Karelia and the Northwestern Region of Russia. ',
+    
+    'welcome_logo'  => '<img src="images/logo_en.png">',
+
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,12 @@
         {{trans('page.site_title')}}
     </x-slot>
 
-    {!!trans('page.welcome_text')!!}
+    {!!trans('page.welcome_text_intro')!!}<br><br>
+    {!!trans('page.welcome_text_content')!!}<br><br>
+    {!!trans('page.welcome_reference_tables')!!}<br><br>
+    {!!trans('page.welcome_text_sources')!!}<br><br>
+    {!!trans('page.welcome_text_software')!!}<br><br>
+    {!!trans('page.welcome_logo')!!}<br><br>
+    
+    
 </x-app-layout>
