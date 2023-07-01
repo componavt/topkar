5f0bb55919b10c42f2a5c30f71464b1b46640fdc
lunata <nataly@krc.karelia.ru>
2023-07-01 12:12:03 +0300

    coords by click

--- resources/views/widgets/leaflet/map_one_toponym.blade.php
+++ resources/views/widgets/leaflet/map_one_toponym.blade.php
@@ -18,5 +18,14 @@
 
       // show a marker on the map
       L.marker({lon:{{$toponym->longitude}} , lat: {{$toponym->latitude}}}).bindPopup('Царевичи').addTo(map);
+      
+    map.on('click', 
+            function(e){
+                    var coord = e.latlng.toString().split(',');
+                    var lat = coord[0].split('(');
+                    var lng = coord[1].split(')');
+                    console.log("You clicked the map at latitude: " + lat[1] + " and longitude:" + lng[0]);
+            });
+
     </script>
 @endif    
