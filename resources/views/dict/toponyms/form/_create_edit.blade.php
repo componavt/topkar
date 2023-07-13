6443d6f0970a34911da67968c75306e80b688dab
lunata <nataly@krc.karelia.ru>
2023-07-13 11:18:54 +0300

    accent

--- resources/views/dict/toponyms/form/_create_edit.blade.php
+++ resources/views/dict/toponyms/form/_create_edit.blade.php
@@ -19,7 +19,7 @@
             ]) 
             </div>
         </div>
-        @if($toponym->accent)
+        @if(!empty($toponym) && $toponym->accent)
             Код ударения: {{$toponym->accent}} 
             (<a href="https://docs.google.com/spreadsheets/d/1jCoNpgLU0w-rUcnPogznJqacoU4EgAZE9bhuG14gGDU/edit?usp=sharing">документация к старым ударениям</a>)<br><br>
         @endif
