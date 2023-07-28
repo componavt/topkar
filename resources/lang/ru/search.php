387c3055c80edbd438639353a02dd4cf080b9b5f
lunata <nataly@krc.karelia.ru>
2023-07-28 16:00:13 +0300

    found_toponyms

--- resources/lang/ru/search.php
+++ resources/lang/ru/search.php
@@ -21,11 +21,11 @@ return [
     'd_f' => 'от',
     'd_t' => 'до',
     'distance' => 'Расстояние',
-    'found_count' => '{0} <span class="important">Не найдено записей.</span>|{1} Найдена <span class="important">:count</span> запись.|[2,4] Найдено <span class="important">:count</span> записи.|[2,*] Найдено <span class="important">:count</span> записей.',
     'for_text_fields' => '<p>В текстовых полях, если Вам нужен неточный поиск, используйте <span class="warning">процент %</span> для замены любого количества символов, '
                       . '<span class="warning">подчеркивание _</span> &nbsp; для замены одного символа.</p>'
                       . '<p>Например, шаблон %ta найдет все строки, оканчивающиеся на ta.</p>',
-    'found_toponyms' => '{0}|{1}, :count топоним|[2,4], :count топонима|[5,Inf], :count топонимов',
+    'found_count' => '{0} <span class="important">Не найдено записей.</span>|{1} Найдена <span class="important">:count</span> запись.|[2,4] Найдено <span class="important">:count</span> записи.|[2,*] Найдено <span class="important">:count</span> записей.',
+    'found_toponyms' => '{0} <span class="important">Не найдено топонимов.</span>|{1} Найден <span class="important">:count</span> топоним.|[2,4] Найдено <span class="important">:count</span> топонима.|[2,*] Найдено <span class="important">:count</span> топонимов.',
     'in_distance' => 'на расстоянии от :from до :to',
     'or' => 'ИЛИ',
     'regex_title' => 'В поле допустимы шаблоны (Regex и собственные). Вы можете использовать следующие обозначения:',
--- resources/views/widgets/found_records.blade.php
+++ resources/views/widgets/found_records.blade.php
@@ -1,5 +1,5 @@
 <p>
-{!!trans_choice('search.found_'.($template ?? 'count'), 
+{!!trans_choice('search.found_'.($template ? $template : 'count'), 
             $n_records%10==0 ? $n_records : ($n_records%100>20 ? $n_records%10  : $n_records%100), 
             ['count'=>number_format($n_records, 0, ',', ' ')])!!}
 </p>
