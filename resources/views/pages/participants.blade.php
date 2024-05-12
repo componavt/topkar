31dad38170b215ba5b70cc10068ed6da6d680dc9
lunata <nataly@krc.karelia.ru>
2024-05-12 22:36:30 +0300

    participants

--- resources/views/pages/participants.blade.php
+++ resources/views/pages/participants.blade.php
@@ -1,50 +1,51 @@
 <?php $locale = LaravelLocalization::getCurrentLocale(); 
     $participants = [
+        [
         'zakharova' => [820, 'illhportal'],
         'mullonen' => [53, 'illhportal'],
         'kongoeva' => null,
-        'khorokhorin' => [1379, 'illhportal'],
-        'nataly' => [22, 'mathem'],
+        ],
+        [
         'andrew' => [804, 'mathem'],
         'shibanova' => [99, 'illhportal'],
+        'khorokhorin' => [1379, 'illhportal'],
+        ],
+        ['nataly' => [22, 'mathem']]
     ];
 ?>
 
-@extends('layouts.page')
+@extends('layouts.master')
 
-@section('page_title')
-{{ trans('navigation.participants') }}
-@endsection
+@section('headTitle', trans('navigation.participants'))
+@section('header', trans('navigation.participants'))
 
 @section('headExtra')
-    {!!Html::style('css/fancybox.css')!!}
+    {!! css('fancybox') !!}
 @stop
 
-@section('body')
-<div class="row">
-    @foreach ($participants as $n => $i)
-    <div class="col-md-4" style="height:570px; text-align: center">
-        <a data-fancybox="gallery" href="/images/participants/big/{{$n}}.jpg" data-caption="{{trans('participant.'.$n.'_name')}}, {{trans('participant.'.$n.'_info')}}">
-            <img class="img-fluid img-responsive img-rounded" src="/images/participants/{{$n}}.jpg" alt=""><br>
-        </a>
-        @if (isset($i[0]))
-        <a href="http://{{$i[1]}}.krc.karelia.ru/member.php?id={{$i[0]}}&plang={{$locale=='en' ? 'e' : 'r'}}">
-        @endif
-        <b>{{trans('participant.'.$n.'_name')}}</b>
-        @if (isset($i[0]))
-        </a>
-        @endif
-        <br>{{trans('participant.'.$n.'_info')}}<br><br>
+@section('main')
+    @foreach ($participants as $chunk)
+    <div class="row">
+        @foreach ($chunk as $n => $i)
+        <div class="col-md-4" style="text-align: center">
+            <a data-fancybox="gallery" href="/photos/big/{{$n}}.jpg" data-caption="{{trans('participant.'.$n.'_name')}}, {{trans('participant.'.$n.'_info')}}">
+                <img class="img-fluid img-responsive img-rounded" src="/photos/{{$n}}.jpg" alt=""><br>
+            </a>
+            @if (isset($i[0]))
+            <a href="http://{{$i[1]}}.krc.karelia.ru/member.php?id={{$i[0]}}&plang={{$locale=='en' ? 'e' : 'r'}}">
+            @endif
+            <b>{{trans('participant.'.$n.'_name')}}</b>
+            @if (isset($i[0]))
+            </a>
+            @endif
+            <br>{{trans('participant.'.$n.'_info')}}<br><br>
+        </div>
+        @endforeach
     </div>
     @endforeach
-</div>
-
 @endsection
 
 @section('footScriptExtra')
-    {!!Html::script('js/fancybox.umd.js')!!}
-@stop
-
-@section('jqueryFunc')
+    {!! js('fancybox.umd') !!}
 @stop
 
