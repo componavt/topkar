190533aa0a91a8b78b7aafcdc2ba885e30135b92
lunata <nataly@krc.karelia.ru>
2025-10-27 20:41:13 +0300

    small

--- resources/views/pages/publications.blade.php
+++ resources/views/pages/publications.blade.php
@@ -5,14 +5,4 @@
     
 @section('main')   
     {!!trans('page.list_of_publications')!!}
-    
-    @if ( 'ru' == app()->getLocale())
-        <div style='margin-left:40px'>    
-        @include('widgets.youtube',
-                    ['width' => '40%',
-                     'height' => '270',
-                     'video' => '7khxXmWET5o'
-                    ])
-        </div>
-    @endif
 @stop
